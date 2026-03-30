551993cfccade06dd7c0e860e436878c61d8b245
hewei <hew@ligeit.com>
2026-03-30 17:43:23 +0800

    Bump version to 1.0.3

--- aiotest/cli.py
+++ aiotest/cli.py
@@ -17,7 +17,7 @@ from aiotest.logger import logger
 from aiotest.shape import LoadUserShape
 from aiotest.users import User
 
-version = "1.0.0"
+version = "1.0.3"
 
 
 def parse_options(args=None):
